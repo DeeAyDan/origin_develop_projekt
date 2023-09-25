5065aac0df0064db1e2537ad5502a04b80335f6d
Daniel <83757480+DeeAyDan@users.noreply.github.com>
2023-09-25 14:36:18 +0200

    Formated with 'Prettier - code formatter'

--- scripts/heroAnimation.js
+++ scripts/heroAnimation.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
-    const image = document.querySelector('#heroImage');
-    image.classList.add('loaded');
+window.addEventListener("load", function () {
+  const image = document.querySelector("#heroImage");
+  image.classList.add("loaded");
 });
